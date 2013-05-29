67937e13a738c4f0f84d0d8125f39dd4ae88ce9c
Luca Fabbri <luca@keul.it>
2013-05-29 18:39:05 +0200

    more tests

--- collective/stringinterp/text/tests/test_subs_adapters.py
+++ collective/stringinterp/text/tests/test_subs_adapters.py
@@ -5,6 +5,8 @@ import unittest
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 
+from plone.app.discussion.comment import Comment
+
 from collective.stringinterp.text.interfaces import ITextExtractor 
 from collective.stringinterp.text.testing import STRING_INTERP_TEXT_INTEGRATION_TESTING
 
@@ -36,3 +38,42 @@ class TestStringSubstitutionAdapter(unittest.TestCase):
         doc = self.layer['portal'].document
         adapter = ITextExtractor(doc)
         self.assertEqual(adapter.text, 'Foo bar baz \n Lorem Ipsum')
+
+
+class TestCommentAdapter(unittest.TestCase):
+
+    layer = STRING_INTERP_TEXT_INTEGRATION_TESTING
+    
+    def test_pacomment_text(self):
+        comment = Comment()
+        comment.text = u'Foo bar baz'
+        adapter = ITextExtractor(comment)
+        self.assertEqual(adapter.text, 'Foo bar baz')
+
+
+class Foo(object):
+    
+    def __init__(self):
+        self.text = ''
+
+
+class TestWhateverObjectAdapter(unittest.TestCase):
+
+    layer = STRING_INTERP_TEXT_INTEGRATION_TESTING
+    
+    def test_pacomment_text(self):
+        obj = Foo()
+        obj.text = 'Foo bar baz'
+        adapter = ITextExtractor(obj)
+        self.assertEqual(adapter.text, 'Foo bar baz')
+
+    def test_invalid_text_info(self):
+        obj = Foo()
+        obj.text = 5
+        adapter = ITextExtractor(obj)
+        self.assertEqual(adapter.text, '')
+
+    def test_invalid_object(self):
+        obj = 31
+        adapter = ITextExtractor(obj)
+        self.assertEqual(adapter.text, '')
